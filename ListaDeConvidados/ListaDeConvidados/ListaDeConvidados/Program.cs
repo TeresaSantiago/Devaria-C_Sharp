e54f038fcda1d8405ffe9d57867eaf614facc336
TeresaSantiago <69803990+TeresaSantiago@users.noreply.github.com>
2021-04-27 17:26:16 -0300

    Update ListaDeConvidados

--- ListaDeConvidados/ListaDeConvidados/ListaDeConvidados/Program.cs
+++ ListaDeConvidados/ListaDeConvidados/ListaDeConvidados/Program.cs
@@ -4,28 +4,76 @@ namespace ListaDeConvidados
 {
     class Program
     {
-        private static string[] ListaDeConvidados =
-        {
-            "Daniel";   // Posição 0
-            "Evandro";  // Posição 1
-            "Matheus";  // Posição 2
-            "Ale";      // Posição 3
-            "Teresa";   // Posição 4
-            "Gabriel";  // Posição 5
-            "Cledson";  // Posição 6
-            "Giuseppe"; // Posição 7
-        };
         static void Main(string[] args)
         {
             Console.WriteLine("Informe o nome do convidado: ");
             var nome = Console.ReadLine();
-            
-            if(nome == null || string.IsNullOrEmpty(nome))
+
+            if (string.IsNullOrEmpty(nome) || string.IsNullOrEmpty(nome))
             {
-                Console.WriteLine("Nome não informado para seguir com o programa");
+                Console.WriteLine("Nome não válido para seguir com o programa");
+                return;
+            }
+
+            Console.WriteLine("Informe a idade do convidado: ");
+            var idadeString = Console.ReadLine();
+            int idade;
+
+            bool idadeInformada = int.TryParse(idadeString, out idade);
+
+            if (idadeInformada == false)
+            {
+                Console.WriteLine("Idade não é válida para seguir com o programa");
                 return;
             }
             
-               
+            bool estaConvidado;
+            switch (nome)
+            {
+                case "Daniel":
+                    estaConvidado = true;
+                    break;
+                case "Evandro":
+                    estaConvidado = true;
+                    break;
+                case "Matheus":
+                    estaConvidado = true;
+                    break;
+                case "Ale":
+                    estaConvidado = true;
+                    break;
+                case "Teresa":
+                    estaConvidado = true;
+                    break;
+                case "Gabriel":
+                    estaConvidado = true;
+                    break;
+                case "Cledson":
+                    estaConvidado = true;
+                    break;
+                case "Giuseppe":
+                    estaConvidado = true;
+                    break;
+                case "Rafael":
+                    estaConvidado = true;
+                    break;
+                default:
+                    estaConvidado = false;
+                    break;
+            }
+
+            if(estaConvidado == true && idade > 18)
+            {
+                Console.WriteLine("Parabéns, seja bem vindo a festa");
+            }
+            else if(estaConvidado == false)
+            {
+                Console.WriteLine("Você não está na lista de convidados, favor verificar com a pessoa que te convidou");
+            }
+            else
+            {
+                Console.WriteLine("Você não tem 18 anos para entrar na festa");
+            }
+        }
     }
 }
